564c62b489f232e420ab502de26364c47c32239e
Abid <abidroid1@gmail.com>
2024-01-21 15:59:16 +0500

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
